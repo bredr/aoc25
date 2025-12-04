fc796b70789b81e98901a3b74b70a17241c6366e
Dave Brett <davidbrett88@gmail.com>
2025-12-04 15:21:52 +0000

    day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -48,3 +48,34 @@ pub fn solve_part1(input: &HashSet<(i32, i32)>) -> i64 {
         })
         .sum()
 }
+
+#[aoc(day4, part2)]
+pub fn solve_part2(input: &HashSet<(i32, i32)>) -> i64 {
+    let mut counter = 0;
+    let mut last_counter = -1;
+    let mut tmp = input.clone();
+    while last_counter != counter {
+        last_counter = counter;
+        tmp.clone().iter().for_each(|(idx, idy)| {
+            if vec![
+                tmp.contains(&(idx + 1, idy + 0)),
+                tmp.contains(&(idx - 1, idy + 0)),
+                tmp.contains(&(idx + 1, idy - 1)),
+                tmp.contains(&(idx + 0, idy - 1)),
+                tmp.contains(&(idx - 1, idy - 1)),
+                tmp.contains(&(idx + 1, idy + 1)),
+                tmp.contains(&(idx + 0, idy + 1)),
+                tmp.contains(&(idx - 1, idy + 1)),
+            ]
+            .iter()
+            .map(|x| if *x { 1 } else { 0 })
+            .sum::<i32>()
+                < 4
+            {
+                tmp.remove(&(idx.clone(), idy.clone()));
+                counter += 1;
+            }
+        });
+    }
+    counter
+}
