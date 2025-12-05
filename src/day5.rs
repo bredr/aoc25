d00ffaba71d57ff288139af3ff5a7180e13de385
Dave Brett <davidbrett88@gmail.com>
2025-12-05 10:33:32 +0000

    day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::ops::RangeInclusive;
 
 #[aoc_generator(day5)]
@@ -41,4 +42,84 @@ pub fn solve_part1(input: &Input) -> usize {
         .iter()
         .filter(|x| input.ranges.iter().any(|range| range.contains(*x)))
         .count()
+}
+
+trait RangeExtended<T> {
+    /// Checks if ranges overlap. If they do, returns the Union (merge) of both.
+    fn merge_if_overlapping(&self, other: &RangeInclusive<T>) -> Option<RangeInclusive<T>>;
+}
+
+impl<T> RangeExtended<T> for RangeInclusive<T>
+where
+    T: Ord + Clone,
+{
+    fn merge_if_overlapping(&self, other: &RangeInclusive<T>) -> Option<RangeInclusive<T>> {
+        if self.start() <= other.end() && other.start() <= self.end() {
+            let new_start = cmp::min(self.start(), other.start());
+            let new_end = cmp::max(self.end(), other.end());
+
+            Some(new_start.clone()..=new_end.clone())
+        } else {
+            None
+        }
+    }
+}
+
+#[aoc(day5, part2)]
+pub fn solve_part2(input: &Input) -> usize {
+    let mut ranges = input.ranges.clone();
+    ranges.sort_by(|a, b| a.start().cmp(b.start()));
+    let mut exclusive_ranges: Vec<RangeInclusive<i64>> = vec![ranges.get(0).unwrap().clone()];
+
+    ranges[1..].iter().for_each(|range| {
+        match exclusive_ranges
+            .clone()
+            .iter()
+            .enumerate()
+            .find(|(idx, r)| match r.merge_if_overlapping(range) {
+                Some(update) => {
+                    exclusive_ranges[*idx] = update.clone();
+                    true
+                }
+                None => false,
+            }) {
+            Some(_) => {}
+            None => {
+                exclusive_ranges.push(range.clone());
+            }
+        }
+    });
+    exclusive_ranges.iter().map(|r| r.clone().count()).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn merge_if_overlapping_test1() {
+        assert_eq!((1..=4).merge_if_overlapping(&(0..=2)), Some(0..=4))
+    }
+
+    #[test]
+    fn merge_if_overlapping_test2() {
+        assert_eq!((1..=4).merge_if_overlapping(&(2..=5)), Some(1..=5))
+    }
+
+    #[test]
+    fn merge_if_overlapping_test3() {
+        assert_eq!((1..=3).merge_if_overlapping(&(4..=5)), None)
+    }
+
+    #[test]
+    fn merge_if_overlapping_test4() {
+        assert_eq!((10..=18).merge_if_overlapping(&(16..=20)), Some(10..=20))
+    }
+
+    #[test]
+    fn solve_part2_example() {
+        let input = input_generator("3-5\n10-14\n16-20\n12-18\n\n1");
+        let result = solve_part2(&input);
+        assert_eq!(result, 14);
+    }
 }
